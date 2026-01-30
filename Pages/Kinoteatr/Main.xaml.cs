012e57a8b77393a8afa5bddf93363e88a39565ad
kenn1d <makarkibanov220@gmail.com>
2026-01-31 00:53:33 +0500

    Реализовано на хорошо. Кибанов

--- Pages/Kinoteatr/Main.xaml.cs
+++ Pages/Kinoteatr/Main.xaml.cs
@@ -11,6 +11,7 @@ namespace WpfApp1.Pages.Kinoteatr
     public partial class Main : Page
     {
         List<KinoteatrContext> AllKinoteatrs = KinoteatrContext.Select();
+
         public Main()
         {
             InitializeComponent();
@@ -24,6 +25,20 @@ namespace WpfApp1.Pages.Kinoteatr
         private void AddRecord(object sender, RoutedEventArgs e)
         {
             MainWindow.init.OpenPage(new Pages.Kinoteatr.Add());
+        }
+
+        private void kinoSearch(object sender, RoutedEventArgs e)
+        {
+            List<KinoteatrContext> search = AllKinoteatrs;
+            if (kinoName.Text != "") search = search.FindAll(x => x.Name == kinoName.Text);
+            if (kinoCountZal.Text != "") search = search.FindAll(x => x.CountZal == int.Parse(kinoCountZal.Text));
+            if (kinoCount.Text != "") search = search.FindAll(x => x.Count == int.Parse(kinoCount.Text));
+
+            parent.Children.Clear();
+            foreach (KinoteatrContext item in search)
+            {
+                parent.Children.Add(new Items.Item(item, this));
+            }
         }
     }
 }
