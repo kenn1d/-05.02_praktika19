09dc359d9b7ff24e670d1f4cc8a96b6836bbca31
kenn1d <makarkibanov220@gmail.com>
2026-01-30 12:27:29 +0500

    Созданные модели. Кибанов

---
+++ Model/Afisha.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class Afisha
+    {
+        public int Id { get; set; }
+        public string IdKinoteatr { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public int Price { get; set; }
+
+        public Afisha(int Id, string IdKinoteatr, string Name, DateTime Time, int Price)
+        {
+            this.Id = Id;
+            this.IdKinoteatr = IdKinoteatr;
+            this.Name = Name;
+            this.Time = Time;
+            this.Price = Price;
+        }
+    }
+}
